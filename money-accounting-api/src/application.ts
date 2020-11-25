caf1084b37f86486d5c74229b7679b309c6550a0
Matias Villalba <matias.gaston.villalba@gmail.com>
2020-11-25 12:36:15 -0300

    code style

--- money-accounting-api/src/application.ts
+++ money-accounting-api/src/application.ts
@@ -2,7 +2,10 @@ import {BootMixin} from '@loopback/boot';
 import {ApplicationConfig} from '@loopback/core';
 import {RepositoryMixin} from '@loopback/repository';
 import {RestApplication} from '@loopback/rest';
-import {RestExplorerBindings, RestExplorerComponent} from '@loopback/rest-explorer';
+import {
+  RestExplorerBindings,
+  RestExplorerComponent,
+} from '@loopback/rest-explorer';
 import {ServiceMixin} from '@loopback/service-proxy';
 import path from 'path';
 import {MySequence} from './sequence';
